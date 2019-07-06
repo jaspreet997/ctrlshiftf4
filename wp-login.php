94b357e3317fc625cd312fe03f13dc0e183b0713
jaspreet <jaspreet.singh4@delhivery.com>
2019-07-06 15:50:04 +0530

    initial commit

---
+++ wp-login.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+	<!--[if IE 8]>
+		<html xmlns="http://www.w3.org/1999/xhtml" class="ie8" lang="en-US">
+	<![endif]-->
+	<!--[if !(IE 8) ]><!-->
+		<html xmlns="http://www.w3.org/1999/xhtml" lang="en-US">
+	<!--<![endif]-->
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+	<title>Log In &lsaquo; Jaspreet Singh &#8212; WordPress</title>
+	<script src="./cdn-cgi/apps/head/aE6vWkBDPeNvOaql7XUgXYc8Y7w.js"></script><link rel='dns-prefetch' href='//s.w.org'/>
+<link rel='stylesheet' href='./wp-admin/load-styles.php?c=0&amp;dir=ltr&amp;load%5B%5D=dashicons,buttons,forms,l10n,login&amp;ver=4.9.10' type='text/css' media='all'/>
+	<meta name='robots' content='noindex,noarchive'/>
+	<meta name='referrer' content='strict-origin-when-cross-origin'/>
+		<meta name="viewport" content="width=device-width"/>
+	<link rel="icon" href="./wp-content/uploads/2017/07/cropped-microsoft-dynamics-nav@2x-32x32.png" sizes="32x32"/>
+<link rel="icon" href="./wp-content/uploads/2017/07/cropped-microsoft-dynamics-nav@2x-192x192.png" sizes="192x192"/>
+<link rel="apple-touch-icon-precomposed" href="./wp-content/uploads/2017/07/cropped-microsoft-dynamics-nav@2x-180x180.png"/>
+<meta name="msapplication-TileImage" content="./wp-content/uploads/2017/07/cropped-microsoft-dynamics-nav@2x-270x270.png"/>
+	</head>
+	<body class="login login-action-login wp-core-ui  locale-en-us">
+		<div id="login">
+		<h1><a href="https://wordpress.org/" title="Powered by WordPress" tabindex="-1">Powered by WordPress</a></h1>
+	
+<form name="loginform" id="loginform" action="./" method="post">
+	<p>
+		<label for="user_login">Username or Email Address<br/>
+		<input type="text" name="log" id="user_login" class="input" value="" size="20"/></label>
+	</p>
+	<p>
+		<label for="user_pass">Password<br/>
+		<input type="password" name="pwd" id="user_pass" class="input" value="" size="20"/></label>
+	</p>
+		<p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever"/> Remember Me</label></p>
+	<p class="submit">
+		<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Log In"/>
+		<input type="hidden" name="redirect_to" value="./wp-admin/"/>
+		<input type="hidden" name="testcookie" value="1"/>
+	</p>
+</form>
+
+<p id="nav">
+	<a href="./?action=lostpassword">Lost your password?</a>
+</p>
+
+<script type="text/javascript">function wp_attempt_focus(){setTimeout(function(){try{d=document.getElementById('user_login');d.focus();d.select();}catch(e){}},200);}
+wp_attempt_focus();if(typeof wpOnload=='function')wpOnload();</script>
+
+	<p id="backtoblog"><a href="./index.html">&larr; Back to Jaspreet Singh</a></p>
+	<div class="privacy-policy-page-link"><a class="privacy-policy-link" href="./privacy-policy/index.html">Privacy Policy</a></div>	
+	</div>
+
+	
+	        <script>if(typeof QTags!=='undefined'){QTags.addButton('pre','pre','<pre>','</pre>\n','p','',105);}</script>
+    	<div class="clear"></div>
+	  <div id="bitnami-banner" data-banner-id="bade1">  <style>#bitnami-banner{z-index:100000;height:80px;padding:0px;width:120px;background:transparent;position:fixed;right:0px;bottom:0px;border:0px solid #ededed}#bitnami-banner .bitnami-corner-image-div{position:fixed;right:0px;bottom:0px;border:0px;z-index:100001;height:110px}#bitnami-banner .bitnami-corner-image-div .bitnami-corner-image{position:fixed;right:0px;bottom:0px;border:0px;z-index:100001;height:110px}#bitnami-close-banner-button{height:12px;width:12px;z-index:10000000000;position:fixed;right:5px;bottom:65px;display:none;cursor:pointer}</style>  <img id="bitnami-close-banner-button" src="./bitnami/images/close.png"/>  <div class="bitnami-corner-image-div">     <a href="./bitnami/index.html" target="_blank">       <img class="bitnami-corner-image" alt="Bitnami" src="./bitnami/images/corner-logo.png"/>     </a>  </div>  <script type="text/javascript" src="./bitnami/banner.js"></script> </div>   </body>
+	</html>
+	
